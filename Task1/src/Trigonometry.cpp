4f621a5e2912dac9fc68da081ca0280b30464481
A <A@mail.ru>
2021-09-30 18:42:53 +0300

    Trigonometic function is working, extern didn't work, test_enable don't need

--- Task1/src/Trigonometry.cpp
+++ Task1/src/Trigonometry.cpp
@@ -8,7 +8,7 @@ double z2(double a, double b){
 	return -4 * pow(sin ((a - b) / 2), 2) * cos (a + b);
 }
 double result(double a, double b){
-	if (z1(a, b) == z2(a, b)) {
+	if (round(z1(a, b)*100)/100 == round(z2(a, b)*100)/100) {
 		return 1;
 	}
 	else{
