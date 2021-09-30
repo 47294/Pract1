c10b1eba97f824bc25ca10eee5e1e03b5cd3e01e
Artem <45859415+47294@users.noreply.github.com>
2021-09-30 18:50:31 +0300

    Update Logic.Tests.cpp

---
+++ Task1/tests/Logic.Tests.cpp
@@ -0,0 +1,13 @@
+extern "C"{}
+#include "../include/Trigonometry.h"
+
+#include <gtest/gtest.h>
+TEST (TestNaRavenstvo, 3e20AND120e5){
+	ASSERT_DOUBLE_EQ(result(3.2, 120.5),1);
+}
+TEST (TestNaRavenstvo, 30i60){
+	ASSERT_DOUBLE_EQ(result(30, 60),1);
+}
+TEST (TestNaRavenstvo, minus20e33333333and195e412){
+	ASSERT_DOUBLE_EQ(result(-20.3333333, 195.412),1);
+}
